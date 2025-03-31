636dc4844e3da0051c6971a2eaec547cbe028049
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-03-31 23:08:20 +0800

    feat: something?

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -1,12 +1,20 @@
-import { createCookieSessionStorage } from "react-router";
+import { createCookieSessionStorage, redirect } from "react-router";
 
-const sessionStorate = createCookieSessionStorage({
-  cookie: {
-    name: "__session",
-    httpOnly: true,
-    path: "/",
-    sameSite: "lax",
-    secrets: [process.env.SESSION_SECRET],
-    secure: process.env.NODE_ENV === "production",
-  },
-});
+type SessionData = {
+  userId: string;
+};
+type SessionFlashData = {
+  error: string;
+};
+
+export const { commitSession, getSession, destroySession } =
+  createCookieSessionStorage({
+    cookie: {
+      name: "__session",
+      httpOnly: true,
+      path: "/",
+      sameSite: "lax",
+      secrets: [process.env.SESSION_SECRET],
+      secure: process.env.NODE_ENV === "production",
+    },
+  });
