44b47d0643152cdf781ed38b2952707847f7c7c5
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-03-31 22:50:09 +0800

    something

---
+++ app/sessions.server.ts
@@ -0,0 +1,12 @@
+import { createCookieSessionStorage } from "react-router";
+
+const sessionStorate = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    httpOnly: true,
+    path: "/",
+    sameSite: "lax",
+    secrets: [process.env.SESSION_SECRET],
+    secure: process.env.NODE_ENV === "production",
+  },
+});
