866811cf8d15b93c200755f25c9db5d3a5735138
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-04-02 09:16:49 +0800

    remove validation messages in zod

--- app/types/auth/signupSchema.ts
+++ app/types/auth/signupSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const signupSchema = z.object({
-  email: z.string().email("Email is required."),
-  username: z.string().min(1, "Username is required."),
-  password: z.string().min(1, "Password is required."),
+  email: z.string().email(),
+  username: z.string(),
+  password: z.string(),
 });
